a2bb1a1220accff450f37ea48c424ca0f2b87ffe
Mao <lcdsmao@gmail.com>
2020-05-12 13:40:34 +0900

    Refactor KiteLiveDataBackedState, get value directly from liveData

---
+++ kite-core/src/main/java/jp/co/cyberagent/kite/KiteLiveDataBackedState.kt
@@ -0,0 +1,73 @@
+package jp.co.cyberagent.kite
+
+import android.os.Looper
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+
+private class KiteLiveDataBackedStateObserver<T>(
+  private val state: KiteState,
+  private val dependencyManager: KiteStateDependencyManager
+) : Observer<T> {
+
+  private var preValue: Any? = Unset
+
+  override fun onChanged(t: T) {
+    if (preValue != t) {
+      preValue = t
+      dependencyManager.notifyDependencyChanged(state)
+    }
+  }
+}
+
+private class KiteLiveDataBackedProperty<T>(
+  private val liveData: MutableLiveData<T>,
+  private val dependencyManager: KiteStateDependencyManager
+) : KiteProperty<T> {
+
+  override var value: T
+    get() {
+      dependencyManager.registerDependency(this)
+      @Suppress("UNCHECKED_CAST")
+      return liveData.value as T
+    }
+    set(value) {
+      if (Looper.getMainLooper() === Looper.myLooper()) {
+        liveData.value = value
+      } else {
+        liveData.postValue(value)
+      }
+    }
+}
+
+private class KiteLiveDataBackedGetter<T>(
+  private val liveData: LiveData<T>,
+  private val dependencyManager: KiteStateDependencyManager
+) : KiteGetter<T> {
+
+  override val value: T
+    get() {
+      dependencyManager.registerDependency(this)
+      @Suppress("UNCHECKED_CAST")
+      return liveData.value as T
+    }
+}
+
+fun <T> KiteDslScope.state(liveData: LiveData<T>): KiteGetter<T> {
+  val state = KiteLiveDataBackedGetter(liveData, stateDependencyManager)
+  val observer = KiteLiveDataBackedStateObserver<T>(state, stateDependencyManager)
+  liveData.observe(lifecycleOwner, observer)
+  return state
+}
+
+fun <T> KiteDslScope.state(liveData: MutableLiveData<T>): KiteProperty<T> {
+  val state = KiteLiveDataBackedProperty(liveData, stateDependencyManager)
+  val observer = KiteLiveDataBackedStateObserver<T>(state, stateDependencyManager)
+  liveData.observe(lifecycleOwner, observer)
+  return state
+}
+
+fun <T> KiteDslScope.state(initialValue: T): KiteProperty<T> {
+  val liveData = componentScopeModel.keepDuringLifecycle { MutableLiveData(initialValue) }
+  return state(liveData)
+}
