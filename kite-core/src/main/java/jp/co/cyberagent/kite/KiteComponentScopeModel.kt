69f89cb4961189a2ece57bf8a2c7e0558865d621
Mao <lcdsmao@gmail.com>
2020-05-15 08:49:36 +0900

    Refactor KiteComponentScopeModel

--- kite-core/src/main/java/jp/co/cyberagent/kite/KiteComponentScopeModel.kt
+++ kite-core/src/main/java/jp/co/cyberagent/kite/KiteComponentScopeModel.kt
@@ -10,15 +10,15 @@ open class KiteComponentScopeModel : ViewModel() {
   private val serviceMap = mutableMapOf<KClass<*>, Any>()
 
   fun <T : Any> addService(service: T, kClass: KClass<T>) {
-    check(kClass !in serviceMap.keys) {
+    check(kClass !in serviceMap) {
       "Service $kClass already added."
     }
     serviceMap[kClass] = service
   }
 
   fun <T : Any> getService(kClass: KClass<T>): T {
-    checkNotNull(serviceMap[kClass]) {
-      "Service $kClass not found, please ensure add it via addService."
+    check(kClass in serviceMap) {
+      "Service $kClass not found, please ensure added it via addService."
     }
     @Suppress("UNCHECKED_CAST")
     return serviceMap[kClass] as T
