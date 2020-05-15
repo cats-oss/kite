5e6aa16e201e64bda79d6db6f90caaa4c5ab64d0
Mao <lcdsmao@gmail.com>
2020-05-15 10:45:44 +0900

    KiteScopeModel.addService become protected

--- kite-core/src/main/java/jp/co/cyberagent/kite/KiteScopeModel.kt
+++ kite-core/src/main/java/jp/co/cyberagent/kite/KiteScopeModel.kt
@@ -9,19 +9,19 @@ open class KiteScopeModel : ViewModel() {
 
   private val serviceMap = mutableMapOf<KClass<*>, Any>()
 
-  fun <T : Any> addService(service: T, kClass: KClass<T>) {
-    check(kClass !in serviceMap) {
-      "Service $kClass already added."
-    }
-    serviceMap[kClass] = service
-  }
-
   fun <T : Any> getService(kClass: KClass<T>): T {
     check(kClass in serviceMap) {
       "Service $kClass not found, please ensure added it via addService."
     }
     @Suppress("UNCHECKED_CAST")
     return serviceMap[kClass] as T
+  }
+
+  protected fun <T : Any> addService(service: T, kClass: KClass<T>) {
+    check(kClass !in serviceMap) {
+      "Service $kClass already added."
+    }
+    serviceMap[kClass] = service
   }
 
   internal fun <T : Any> createTagIfAbsent(key: Any, creator: () -> T): T {
