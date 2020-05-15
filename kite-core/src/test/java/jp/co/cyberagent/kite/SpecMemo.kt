bf06e04fb16c8e20d451baab2db3f979f87721b8
Mao <lcdsmao@gmail.com>
2020-05-15 18:24:44 +0900

    Spec memo

---
+++ kite-core/src/test/java/jp/co/cyberagent/kite/SpecMemo.kt
@@ -0,0 +1,23 @@
+package jp.co.cyberagent.kite
+
+import io.kotlintest.Spec
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+fun <T : Any> Spec.memo(init: () -> T): ReadOnlyProperty<Nothing?, T> {
+  var value: T? = null
+
+  beforeTest {
+    value = init()
+  }
+
+  afterTest {
+    value = null
+  }
+
+  return object : ReadOnlyProperty<Nothing?, T> {
+    override fun getValue(thisRef: Nothing?, property: KProperty<*>): T {
+      return value!!
+    }
+  }
+}
