ddf6b533d3d6c7528186398782f46b910053dbba
Mao <lcdsmao@gmail.com>
2020-05-17 17:45:32 +0900

    KiteStateKey

--- kite-runtime/src/main/java/jp/co/cyberagent/kite/internal/KiteStateKeyGenerator.kt
+++ kite-runtime/src/main/java/jp/co/cyberagent/kite/internal/KiteStateKeyGenerator.kt
@@ -4,16 +4,18 @@ import java.util.concurrent.atomic.AtomicInteger
 import jp.co.cyberagent.kite.KiteDslScope
 import jp.co.cyberagent.kite.setIfAbsent
 
+private data class KiteStateKey(val num: Int)
+
 internal class KiteStateKeyGenerator {
 
   private val stateKeyGenerator = AtomicInteger(0)
 
-  fun createStateKey(): String {
-    return "KiteStateKeyGenerator_${stateKeyGenerator.incrementAndGet()}"
+  fun createStateKey(): Any {
+    return KiteStateKey(stateKeyGenerator.getAndDecrement())
   }
 }
 
-internal fun KiteDslScope.createStateKey(): String {
+internal fun KiteDslScope.createStateKey(): Any {
   val generator = setIfAbsent(KiteStateKeyGenerator::class) { KiteStateKeyGenerator() }
   return generator.createStateKey()
 }
