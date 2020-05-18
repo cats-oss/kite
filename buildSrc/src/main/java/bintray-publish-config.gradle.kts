b7af05b13a28290eac7ac28042915f1514673db1
Mao <lcdsmao@gmail.com>
2020-05-18 22:23:54 +0900

    Use kotlin dsl for bintray config

--- buildSrc/src/main/java/bintray-publish-config.gradle.kts
+++ buildSrc/src/main/java/bintray-publish-config.gradle.kts
@@ -1,25 +1,25 @@
 import com.jfrog.bintray.gradle.BintrayExtension
+import java.util.Date
 
 plugins {
   id("com.jfrog.bintray")
 }
 
-project.afterEvaluate {
-  extensions.configure<BintrayExtension> {
-    user = System.getProperty("bintrayUser")
-    key = System.getProperty("bintrayKey")
-    setPublications("maven")
+bintray {
+  user = System.getProperty("bintrayUser")
+  key = System.getProperty("bintrayKey")
+  setPublications("maven")
 
-    pkg(closureOf<BintrayExtension.PackageConfig> {
-      repo = "maven"
-      userOrg = "cats-oss"
-      name = extra["POM_ARTIFACT_ID"].toString()
-      vcsUrl = extra["POM_SCM_URL"].toString()
-      websiteUrl = extra["POM_URL"].toString()
-      setLicenses("Apache-2.0")
-      version(closureOf<BintrayExtension.VersionConfig> {
-        name = extra["VERSION_NAME"].toString()
-      })
+  pkg(closureOf<BintrayExtension.PackageConfig> {
+    repo = "maven"
+    userOrg = "cats-oss"
+    name = extra["POM_ARTIFACT_ID"].toString()
+    vcsUrl = extra["POM_SCM_URL"].toString()
+    websiteUrl = extra["POM_URL"].toString()
+    setLicenses("Apache-2.0")
+    version(closureOf<BintrayExtension.VersionConfig> {
+      name = extra["VERSION_NAME"].toString()
+      released = Date().toString()
     })
-  }
+  })
 }
