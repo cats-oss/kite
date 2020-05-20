70daca9038725cfb1e93ccad4fef5800cd088ee0
Mao <lcdsmao@gmail.com>
2020-05-20 14:00:13 +0900

    Add KiteDslScope documentation

--- kite-core/src/main/java/jp/co/cyberagent/kite/core/KiteDslScope.kt
+++ kite-core/src/main/java/jp/co/cyberagent/kite/core/KiteDslScope.kt
@@ -2,11 +2,21 @@ package jp.co.cyberagent.kite.core
 
 import kotlinx.coroutines.CoroutineScope
 
+/**
+ * Define a scope for kite DSL. All kite DSL (like [state], [memo], etc) is an extension
+ * on [KiteDslScope]. [KiteDslScope] inherits [CoroutineScope] so you start coroutine via [launch],
+ * and [async]. [KiteDslScope] also inherits [KiteContext] and usually the behavior of [KiteContext]
+ * should delegate to the property [KiteDslScope.kiteContext].
+ */
 @KiteDslMaker
 interface KiteDslScope : CoroutineScope, KiteContext {
   val kiteContext: KiteContext
 }
 
+/**
+ * A simple implementation for [KiteDslScope] with using delegate pattern. A default
+ * [KiteCoroutineDispatchers] instance will be set into the [KiteContext] if absent.
+ */
 private class KiteDsScopeImpl(
   coroutineScope: CoroutineScope,
   override val kiteContext: KiteContext
@@ -19,6 +29,13 @@ private class KiteDsScopeImpl(
   }
 }
 
+/**
+ * Create a simple [KiteDslScope] from [coroutineScope] and [kiteContext].
+ *
+ * It's not recommend directly use this method to write kite DSL. Instead, you can use
+ * [Fragment.kiteDsl] or [ComponetntActivity.kiteDsl] in production code. And use [TestKiteDslScope]
+ * or [runTestKiteDsl] in test code.
+ */
 @Suppress("FunctionName")
 fun KiteDslScope(
   coroutineScope: CoroutineScope,
