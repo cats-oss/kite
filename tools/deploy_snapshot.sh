77fb7900dec16ffcfca0acdd4d1c3c8708e6bd0d
Mao <lcdsmao@gmail.com>
2020-05-31 17:01:13 +0900

    Add deploy snapshot job to workflows.

---
+++ tools/deploy_snapshot.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+REPOSITORY="cats-oss/kite"
+REF="refs/heads/master"
+
+set -e
+
+VERSION_NAME=$(grep VERSION_NAME gradle.properties)
+
+if [[ ! "$VERSION_NAME" =~ "SNAPSHOT" ]]; then
+  echo "Skipping snapshot deployment: wrong version name '$VERSION_NAME'."
+elif [[ "$GITHUB_REPOSITORY" != "$REPOSITORY" ]]; then
+  echo "Skipping snapshot deployment: wrong repository. Expected '$REPOSITORY' but was '$GITHUB_REPOSITORY'."
+elif [[ "$GITHUB_EVENT_NAME" != "push" ]]; then
+  echo "Skipping snapshot deployment: was '$GITHUB_EVENT_NAME'."
+elif [[ "$GITHUB_REF" != "$REF" ]]; then
+  echo "Skipping snapshot deployment: wrong ref. Expected '$REF' but was '$GITHUB_REF'."
+else
+  echo "Deploying kite..."
+  ./gradlew clean artifactoryPublish -DartifactoryUser="$ARTIFACTORY_USER" -DartifactoryPassword="$ARTIFACTORY_PASSWORD"
+  echo "Store deployed!"
+fi
